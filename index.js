22b658d95ca4e1f8ed7e7a2c3c85e3b343820520
pilgrim1807 <51112693+pilgrim1807@users.noreply.github.com>
2024-10-14 17:15:47 +0300

    1 и 2 задания

---
+++ index.js
@@ -0,0 +1,14 @@
+// JavaScript-код с созданием переменных
+/*Автор:
+Языкова Анастасия*/
+
+let myName = 'Anastasiya';
+let mySurnamу = 'Yazykova';
+let myFavoriteDrink ='Green tea with milk and honey';
+let myFavoriteAnimal = 'Spider';
+let myFavoriteProgrammingLanguage = 'JavaScript';
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage)
