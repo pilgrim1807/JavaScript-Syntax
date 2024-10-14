2eb0242779cec39efedca18b59310da12a37ccfa
pilgrim1807 <51112693+pilgrim1807@users.noreply.github.com>
2024-10-14 17:28:04 +0300

    А это действительно 3 и 4 задания

---
+++ car.js
@@ -0,0 +1,18 @@
+const carName ='Porsche 356';
+let maxCarSpeed = '201 km/h';
+maxCarSpeed = '230 km/h';
+console.log('У машины изменилась скорость', maxCarSpeed);
+maxCarSpeed = '250 km/h';
+console.log('У машины изменилась скорость', maxCarSpeed);
+maxCarSpeed = '280 km/h';
+console.log('У машины изменилась скорость', maxCarSpeed);
+let carOwner = 'Adam Levine';
+carOwner = 'Jane Herman';
+console.log('У машины изменился владелец', carOwner);
+carOwner = 'Behati Prinsloo';
+console.log('У машины изменился владелец', carOwner);
+carOwner = 'Dusty Rose Levine';
+console.log('У машины изменился владелец', carOwner);
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner)
